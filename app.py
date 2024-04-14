e6f5bcfc35941d2ea8ae8ef1c948877e0007b949
PalacinkaVelika <128801641+PalacinkaVelika@users.noreply.github.com>
2024-04-14 20:39:28 +0200

    hodina zwei

--- app.py
+++ app.py
@@ -65,6 +65,7 @@ for prvek, kolikaty in enumerate(list):
  print(kolikaty) # 0, 1, 2, 3, ...
  
 '''
+'''
 a = 7
 while a > 0:
     print(a)
@@ -77,3 +78,93 @@ while True:
     a -= 1
     if a < 0:
         break
+'''
+'''
+def rectangle_info(width, height):
+    plocha = width * height
+    perimetr = 2 * (width + height)
+    return plocha, perimetr
+
+a, b = rectangle_info(4, 6)
+perimetr = rectangle_info(4, 6)[1]
+print(perimetr)
+'''
+'''
+def greet(name):
+    if name == "":
+        print("prosím zadej platné jméno")
+        return
+    if name == "Josef":
+        print("dobrý jméno")
+    return "Hello, " + name
+
+print(greet("John"))
+print(greet(""))
+'''
+'''
+radek_kalkulacky = "2+2/3"
+radek_kalkulacky += "-"
+radek_kalkulacky += "5"
+print(radek_kalkulacky)
+vysledek = eval(radek_kalkulacky)
+print(vysledek)
+vysledek = 2+2/3-5
+print(vysledek)
+'''
+'''
+def add(x: int, y: int) -> str:
+    return x+y
+
+result = add(2, 5)
+print(result)
+'''
+'''
+a = "3"
+b = "5"
+print(int(a+b))
+'''
+'''
+def krasna_fce():
+ print("funguju")
+def main():
+ print("Hello from main function!")
+if __name__ == "__main__":
+    main()
+    krasna_fce()
+'''
+'''
+a = False
+
+if a == True:
+    x = "lets go"
+else:
+    x = "no go"
+
+x = "lets go" if a else "no go" 
+'''
+'''
+# setup
+def func(par):
+ return par + 25
+arr = [1,5,6,4,2]
+# fUnKcIoNáLnÍ programko
+x = [func(a) for a in arr]
+print(x) # [26, 30, 31, 29, 27]
+# realisticky normální programování verze
+x = []
+for a in arr:
+ x.append(func(a)) # vše v tomto bloku se stane pro každý prvek listu "arr"
+print(x) # [26, 30, 31, 29, 27]
+'''
+'''
+vysledek = (lambda x, y: x + y)(1,2) #3
+print(vysledek)
+
+arr = [1,5,6,4,2]
+x = [(lambda a: a + 25)(a) for a in arr]
+print(x)
+'''
+
+arr = [1,"5",6.4156,4,2]
+x = list(map(lambda a: int(a), arr))
+print(x) # [1, 5, 6, 4, 2]
